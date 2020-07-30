a4a52f76cfdfb8a2402d58c11a6d00a054251656
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-07-30 17:00:35 +0900

    SL Update: Lesson 7/30

--- PG/testcode3.py
+++ PG/testcode3.py
@@ -4,3 +4,11 @@ sys.stdin = open("input.txt", "r")
 #! TEST CODE 3
 # TODO ==============================================================
 # TODO START CODE HERE:
+
+
+n = int(input())
+x = [list(input()) for _ in range(n)]
+
+# [['0', '0', '1', '1'], ['0', '1', '1', '0']]
+
+print(x)
