a1d5edf5e8cdd3bbee8364af44a3748622071ed1
JC Tinio <tinio.johnchristian@gmail.com>
2020-08-07 21:04:57 +0900

    PL Update: Review 8/7

--- PG/testcode3.py
+++ PG/testcode3.py
@@ -4,3 +4,15 @@ sys.stdin = open("input.txt", "r")
 #! TEST CODE 3
 # TODO ==============================================================
 # TODO START CODE HERE:
+
+# l = []
+
+for i in range(1, 11): 
+    for j in range(i + 1): 
+        # l.append("*" * j)
+        print("*" * j)
+    
+
+
+
+# print("\n".join(l))
