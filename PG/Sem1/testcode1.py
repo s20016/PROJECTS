6f154673a97125ff23800d3d3d751d57dce7c99e
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-10-05 15:30:00 +0900

    SL: Lesson 10/5

--- PG/Sem1/testcode1.py
+++ PG/Sem1/testcode1.py
@@ -4,8 +4,3 @@ sys.stdin = open("PG/Sem1/input.txt", "r")
 #! TEST CODE 1
 # TODO ==============================================================
 # TODO START CODE HERE:
-
-n = int(input())
-x = [int(input()) for _ in range(7)]
-
-print(n if sum(x) >= n else sum(x))
