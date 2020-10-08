3b880bb92a87f6ebcbfeb0710c246bbae46eacdd
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-10-08 16:59:17 +0900

    SL: Lesson 10/8

--- PG/Python/Sem1/testcode1.py
+++ PG/Python/Sem1/testcode1.py
@@ -1,11 +1,24 @@
 import sys
-sys.stdin = open("myProjects/PG/Python/Sem1/input.txt", "r")
-sys.stdout = open("myProjects/PG/Python/Sem1/output.txt", "w")
+sys.stdin = open("Python/Sem1/input.txt", "r")
+sys.stdout = open("Python/Sem1/output.txt", "w")
 #! TEST CODE 1
 # TODO ==============================================================
 # TODO START CODE HERE:
 
-import collections
+x, y = list(map(int, input().split()))
+n = '{:0{}}'.format(min(x, y), len(str(max(x, y))))
 
-x = collections.Counter(input())
-print("OK" if len(x.keys()) == 4 else "NG")
+a = [str(max(x, y)), n]
+m = 1
+l = []
+l2 = []
+
+for n in range(len(str(max(x, y)))):
+    l.append((int(a[0][-m]) + int(a[1][-m])))
+    m += 1
+l.reverse()
+
+for n in l:
+    l2.append(str(n)[-1])
+
+print("".join(l2))
