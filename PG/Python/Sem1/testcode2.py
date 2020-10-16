6f904baf8b430f55ea25c770c179015dfbbbe625
JC Tinio <tinio.johnchristian@gmail.com>
2020-10-16 21:34:39 +0900

    PL: Updated ch04 and README.md

--- PG/Python/Sem1/testcode2.py
+++ PG/Python/Sem1/testcode2.py
@@ -1,6 +1,15 @@
 import sys
-sys.stdin = open("myProjects/PG/Python/Sem1/input.txt", "r")
-sys.stdout = open("myProjects/PG/Python/Sem1/output.txt", "w")
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
 #! TEST CODE 2
 # TODO ==============================================================
 # TODO START CODE HERE:
+
+import sys
+
+x = list(sys.stdin.read().split("\n"))
+l = []
+for i in x:
+    l.append(f"<li>{i}</li>\n")
+
+print(*l)
