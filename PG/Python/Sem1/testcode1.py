ed0499a5917f1624381460070585cdf71ce35c3c
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-10-28 07:59:41 +0900

    SL: Lesson 10/28

--- PG/Python/Sem1/testcode1.py
+++ PG/Python/Sem1/testcode1.py
@@ -5,14 +5,4 @@ sys.stdout = open("output.txt", "w")
 # TODO ==============================================================
 # TODO START CODE HERE:
 
-x, y = list(map(str, input().split()))
-
-date = list(x.split("/"))
-time = list(y.split(":"))
-
-A = int(time[0]) // 24
-B = int(time[0]) % 24
-print(A, B)
-h = str(abs(A - B - 1)).zfill(2)
-
-print(f"{date[0]}/{int(date[1]) + A}", f"{h}:{time[1]}")
+print(input())
