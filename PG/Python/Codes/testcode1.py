bd0f2ea16879d2a1765c12f61ccf47564ede017b
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-11-07 21:33:03 +0900

    SL: Update

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -1,11 +1,11 @@
 import sys
 
-sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
-sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
+sys.stdin = open("/home/s20016/PROJECTS/PG/Python/Codes/input.txt", "r")
+sys.stdout = open("/home/s20016/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
 
 x = input()
 for i in range(5):
-    print("Hello {x}")
+    print(f"Hello {x}")
