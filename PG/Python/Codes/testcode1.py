772fa66cf0c44c4cbf38bf1ae162b72a81d2a02b
Czekras <tinio.johnchristian@gmail.com>
2020-11-07 12:28:13 +0900

    PL: Created new bash alias

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -1,7 +1,7 @@
 import sys
 
-sys.stdin = open("$HOME/PROJECTS/PG/Python/Codes/input.txt", "r")
-sys.stdout = open("$HOME/PROJECTS/PG/Python/Codes/output.txt", "w")
+sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
+sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
