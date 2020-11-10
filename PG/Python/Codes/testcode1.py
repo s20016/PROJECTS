b3aa3838b4ca68afef1e74f5378b68835eeefecf
s20016 <s20016@std.it-college.ac.jp>
2020-11-10 14:27:33 +0900

    SL: Testing ssh key in SF313-51U

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -5,7 +5,3 @@ sys.stdout = open("/home/s20016/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
-
-x = input()
-for i in range(7):
-    print(f"Hello {x}")
