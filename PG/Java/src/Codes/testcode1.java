1b71502d48a27e4c2c92aac0110c65adf8da6449
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-11-10 12:16:02 +0900

    SL: Java Lesson 11/10

--- PG/Java/src/Codes/testcode1.java
+++ PG/Java/src/Codes/testcode1.java
@@ -1,9 +1,23 @@
 package Codes;
 
+import java.util.Scanner;
+
 public class testcode1 {
     public static void main(String[] args) {
-    
-       System.out.println("Hello World!");
+        Scanner scan = new Scanner (System.in);
 
+// Lesson 11/10
+//        int x = scan.nextInt();
+//        int y = scan.nextInt();
+//        if ( x < y && ( x % 2 ) == 0 && ( y % 2 ) == 0 )
+//            System.out.println( "xはyより小さく、かつ、xとyは共に偶数である。" );
+//        if ( x == y && x < 0 )
+//            System.out.println( "xとyは等しく、かつ、負の数である。" );
+//        if ( ( x <= 10 || x >= 100 ) && ( y >= 10 && y <= 100 ) )
+//                System.out.println( "xは10以下または100以上で、かつ、yは10以上かつ100以下である。" );
+//        if ( ! ( x < 0 && y < 0 ) )
+//                System.out.println( "xもyも負の数である、ではない。" );
+
+        scan.close();
     }
 }
