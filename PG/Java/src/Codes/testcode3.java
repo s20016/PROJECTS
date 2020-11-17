0e070756687ed50a81320208bf9c09a0c62f4328
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-11-17 18:08:34 +0900

    SL: Lesson 11/17

--- PG/Java/src/Codes/testcode3.java
+++ PG/Java/src/Codes/testcode3.java
@@ -3,7 +3,9 @@ package Codes;
 public class testcode3 {
     public static void main(String[] args) {
     
-
+	int a = 65;  
+	int b = -135;  
+	System.out.println("The sum of a and b is = " + Integer.sum(a, b));
 
     }
 }
--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -5,3 +5,18 @@ sys.stdout = open("/home/s20016/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
+
+list1 = [-3, -2, -1, 0, 1, 2, 3]
+list2 = []
+for i in list1:
+    if i < 0:
+        list2.append("負")
+    elif i == 0:
+        list2.append("零")
+    else:
+        list2.append("正")
+
+print(list2)
+
+list1.reverse()
+print(list1)
