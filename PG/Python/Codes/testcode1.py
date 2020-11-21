6d5839af42c88dab75d31cd7613e8ff1a0b50dff
Czekras <tinio.johnchristian@gmail.com>
2020-11-21 21:24:24 +0900

    PL: Updated [Project 01]

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -6,10 +6,6 @@ sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # TEST CODE 1
 # =========================================================================
 
-# int [] a = {1, 3, 4, 7, 9, 11};
-# int idx = 2;
-# int x = 99;
-
-a = [1, 3, 4, 7, 9, 11]
-a[2] = 99
-print(a)
+x = sys.stdin.read().split("\n")
+for i in x:
+    print(f"<li>{i}</li>")
