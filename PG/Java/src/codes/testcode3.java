217662488953ccbdec3f0f07b1a6b32f84a41bd1
Czekras <tinio.johnchristian@gmail.com>
2020-12-01 21:51:40 +0900

    PL: Review 12/01

--- PG/Java/src/codes/testcode3.java
+++ PG/Java/src/codes/testcode3.java
@@ -1,42 +1,17 @@
 package codes;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class testcode3 {
 
 	public static void main(String[] args) {
-		int [] a = {1, 3, 4, 7, 9, 11};
-		int idx = 0;
-		int n = 7;
-		// int [] expected = {};
-		int [] ret;
 
-    	if (n <= 0 || idx < 0) {
+        System.out.println("What is your name?");
 
-    		ret = new int[a.length];
-    		for (int i = 0; i < a.length; i++) {
-    			ret[i] = a[i];
-			}
-			
-    	} else {
-    		if (idx + n >= a.length) {
-    			if (n > a.length - idx ) {
-    				ret = new int[a.length - idx];
-    			} else {
-    				ret = new int[a.length];
-    			}
-    		} else {
-    			ret = new int[a.length - n];
-    		}
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
 
-    		for (int i = 0, j = 0; i < a.length; i++) {
-    			if ( i < idx || i >= idx + n) {
-    				ret[j++] = a[i];
-    			}
-    		}
-    	}
-
-		// return ret;
-		System.out.println(Arrays.toString(ret));
+        System.out.println("Hello " + name);
 	}
 }
