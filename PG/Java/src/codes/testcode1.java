015e795dbbc52c018888ce7db921233ddc7ac812
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-12-03 13:45:49 +0900

    SL: Lesson 12/13

--- PG/Java/src/codes/testcode1.java
+++ PG/Java/src/codes/testcode1.java
@@ -4,10 +4,18 @@ public class testcode1 {
 
 	public static void main(String[] args) {
 
-		int a = 0;
-		System.out.println(a);
-		
-		int b = ++a;
-		System.out.println(b);
+        for (int k = 0; k < 3; k++) {
+            System.out.println("***");
+            for (int j = 0; j < 1; j++) {
+                System.out.println("**");
+                for (int i = 0; i < 1; i++) {
+                    System.out.println("*");
+                }
+            System.out.println("**");
+            }
+        System.out.println("***");
+        System.out.println();
+        }
+
     }
 }
