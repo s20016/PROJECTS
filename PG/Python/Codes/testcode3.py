6955bd56ae7ad3fefd8fb0a3ee74e97bbfb88dc3
John Christian Tinio <s20016@std.it-college.ac.jp>
2020-12-03 13:48:40 +0900

    SL: Lesson 12/13

--- PG/Python/Codes/testcode3.py
+++ PG/Python/Codes/testcode3.py
@@ -1,7 +1,7 @@
 import sys
 
-sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
-sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
+sys.stdin = open("/home/s20016/PROJECTS/PG/Python/Codes/input.txt", "r")
+sys.stdout = open("/home/s20016/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 3
 # =========================================================================
