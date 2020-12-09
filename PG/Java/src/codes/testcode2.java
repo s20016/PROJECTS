df60631989a96ff4ef319bf8dda37259449b3216
Czekras <tinio.johnchristian@gmail.com>
2020-12-09 13:20:55 +0900

    PL: Lesson 12/09

--- PG/Java/src/codes/testcode2.java
+++ PG/Java/src/codes/testcode2.java
@@ -1,9 +1,17 @@
 package codes;
 
+import java.util.Scanner;
+
 public class testcode2 {
 	
 	public static void main(String[] args ) {
 	
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a num: ");
+		int a = scan.nextInt();
 		
+		System.out.println(10 + a);
+		
+		scan.close();
 	}
 }
--- PG/Java/src/notes/PG3_Sem4_1.java
+++ PG/Java/src/notes/PG3_Sem4_1.java
@@ -1,11 +1,12 @@
 package notes;
 
+import java.util.Scanner;		// usingScanner();
+
 public class PG3_Sem4_1 {
 
 	public static void main(String[] args) {
 	
-		whileLoop();
-		doWhileLoop();
+		usingScanner();
 	}
 
 	/*
@@ -16,6 +17,12 @@ public class PG3_Sem4_1 {
 	 * 		doWhileLoop();
 	 * 		breakInLoop();
 	 * 		breakInNestedLoop();
+	 * 
+	 * Lesson 5 (12-09-2020)
+	 * 		finalVar();
+	 * 		printInJava();
+	 * 		intLong();
+	 * 		usingScanner();
 	 */
 	
 	static void forLoop() {
@@ -71,5 +78,51 @@ public class PG3_Sem4_1 {
 		}
 	}
 	
+	static void finalVar() {
+		final int finalVar;
+		finalVar = 10;		// Assigns the final value
+//		finalVar = 20;		// Error. finalVar already assigned
+		System.out.println(finalVar);
+	}
+	
+	static void printInJava() {
+		System.out.println("println = ends with new line");
+		
+		System.out.print("print = ");
+		System.out.println("conitnuation of previous print");
+		
+		String word = "number"; int num = 10;
+		System.out.printf("The %s is %d!\n", word, num);
+		
+		// \n = new line
+		// s – formats strings
+		// d – formats decimal integers
+		// f – formats the floating-point numbers
+		// t – formats date/time values
+		
+		System.out.printf("%s ", word);		// string as it is
+		System.out.printf("%S\n", word);		// capitalized
+		
+		System.out.printf("%.6s", "0123456789InString"); // prints [:6]
+	}
+	
+	static void intLong() {
+		int x = 10; 
+		long y = 20;
+//		int sum = x + y;	// cannot convert long to int error
+		long sum = x + y;	// works because long > int
+		System.out.println(sum);
+	}
+	
+	static void usingScanner() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a num: ");
+		int a = scan.nextInt();
+		
+		String ret = (a > 10) ? "BIG" : (a < 10) ? "SMALL" : "EQUAL";
+		System.out.println(ret);
+		
+		scan.close();
+	}
 	
 }
