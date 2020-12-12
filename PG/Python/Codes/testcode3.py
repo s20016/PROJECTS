771919f484f3eab1e361fb81d70c388d08f1ae17
Czekras <tinio.johnchristian@gmail.com>
2020-12-12 21:57:11 +0900

    PL: Added movie marker in [P01]

--- PG/Python/Codes/testcode3.py
+++ PG/Python/Codes/testcode3.py
@@ -8,38 +8,50 @@ sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # TEST CODE 3
 # =========================================================================
 
+# movie - dt
+# normal - dd
+
 mainList = list(sys.stdin.read().split("\n"))
 rewatchList = []
 movieList = []
 mainListSort = []
 finalMainList = []
 
-for i in mainList:
-    if i[:4] == "(2) ":
-        rewatchList.append(i[4:])
-        mainListSort.append(i[4:])
-    else:
-        mainListSort.append(i)
+# for i in mainList:
+#     if i[:4] == "(2) ":
+#         rewatchList.append(i[4:])
+#         mainListSort.append(i[4:])
+#     else:
+#         mainListSort.append(i)
 
-for i in mainListSort:
-    if i[-4:] == " (M)":
-        movieList.append(i[:-4])
+# for i in mainListSort:
+#     if i[-4:] == " (M)":
+#         movieList.append(i[:-4])
+#         mainListSort.append(i[:-4])
+
+for i in mainList:
+    if "(M)" in i:
+        newI = i.replace("(M)", "").strip()
+        finalMainList.append(f"<dt>{newI}</dt>")
+    else:
+        finalMainList.append(f"<dd>{i}</dd>")
+
 
 # print(mainList)
 # print(len(mainListSort))
 
-mainListSort.sort()
-# print("\n".join(i for i in mainListSort))
-
-for i in mainListSort:
-    if i in rewatchList:
-        finalMainList.append(f"(2) {i}")
-    else: 
-        finalMainList.append(i)
-
+# mainListSort.sort()
 print("\n".join(i for i in finalMainList))
+
+# for i in mainListSort:
+#     if i in movieList:
+#         finalMainList.append(f"<dt>{i}</dt>")
+#     else: 
+#         finalMainList.append(f"<dd>{i}</dd>")
+
+# print("\n".join(i for i in finalMainList))
 # print("\n".join(f"<li>{i}</li>" for i in finalMainList))
 
-# print(len(rewatchList))     # 11 Rewatch
+# print(len(rewatchList))     # 12 Rewatch
 # print(len(movieList))       # 27 Movies
 # print(len(finalMainList))   # 203 Anime
