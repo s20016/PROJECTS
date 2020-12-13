a08c4f864cf524851ac38f3665e64a246cc4326b
Czekras <tinio.johnchristian@gmail.com>
2020-12-13 17:29:26 +0900

    PL: Modified [P01]

--- PG/Python/Codes/testcode3.py
+++ PG/Python/Codes/testcode3.py
@@ -30,10 +30,19 @@ finalMainList = []
 #         mainListSort.append(i[:-4])
 
 for i in mainList:
-    if "(M)" in i:
+    if i[-4:] == " (M)":
         newI = i.replace("(M)", "").strip()
+        if i[:4] == "(2) ":
+            newI2 = newI.replace("(2)", "").strip()
+            movieList.append(newI2)
+            finalMainList.append(f'<li class="M">{newI2}<span class="R">2</span></li>')
+            continue
         finalMainList.append(f'<li class="M">{newI}</li>')
         movieList.append(newI)
+    elif i[:4] == "(2) ":
+        newI3 = i.replace("(2)", "").strip()
+        rewatchList.append(i[4:])
+        finalMainList.append(f'<li>{newI3}<span class="R">2</span></li>')
     else:
         finalMainList.append(f"<li>{i}</li>")
 
@@ -42,7 +51,7 @@ for i in mainList:
 # print(len(mainListSort))
 
 # mainListSort.sort()
-print("\n".join(i for i in finalMainList))
+# print("\n".join(i for i in finalMainList))
 
 # for i in mainListSort:
 #     if i in movieList:
@@ -50,9 +59,13 @@ print("\n".join(i for i in finalMainList))
 #     else: 
 #         finalMainList.append(f"<dd>{i}</dd>")
 
-# print("\n".join(i for i in finalMainList))
-# print("\n".join(f"<li>{i}</li>" for i in finalMainList))
-
 # print(len(rewatchList))     # 12 Rewatch
 # print(len(movieList))       # 27 Movies
 # print(len(finalMainList))   # 203 Anime
+
+# print(rewatchList)    # 12 Rewatch
+# print(movieList)      # 27 Movies
+# print(finalMainList)  # 203 Anime
+
+print("\n".join(i for i in finalMainList))
+# print("\n".join(f"<li>{i}</li>" for i in finalMainList))
