e18bdcbd97eb7f469b7ea55189be10b925f84b05
s20016 <s20016@std.it-college.ac.jp>
2020-12-17 17:03:26 +0900

    SL: Lesson 12/17

--- PG/JavaScript/Codes/testcode2.js
+++ PG/JavaScript/Codes/testcode2.js
@@ -1,7 +1,17 @@
 (stdin => {
-  const inputs  = stdin.toString().trim().split('\n')
-  const line = inputs[0]
+  const inputs = stdin.toString().trim().split('\n')
+  const l = []
+
+  for (i = 1; i < Number(inputs) + 1; i++) {
+    const x = (i % 3 === 0 & i % 5 === 0)
+      ? l.push("Fizz Buzz")
+      : (i % 3 === 0) 
+      ? l.push("Fizz")
+      : (i % 5 === 0)
+      ? l.push("Buzz") : i
+  }
+  
   // Display
-  console.log(inputs[0] * inputs[1])
+  console.log(l)
 
 })(require('fs').readFileSync('/dev/stdin', 'utf8'))
