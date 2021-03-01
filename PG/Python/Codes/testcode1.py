0d8bcd3098bf661fbd29fa9933c28a2219b4d3e6
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-03-01 17:43:32 +0900

    Test: os.getcwd in vim session

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -1,8 +1,8 @@
-import sys, os
+import os
+import sys
 
 sys.stdin = open(f"{os.getcwd()}/input.txt", "r")
 sys.stdout = open(f"{os.getcwd()}/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
-
