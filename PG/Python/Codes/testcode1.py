4ce046e3b8d1e77add9201854982b426a7307926
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-03-03 14:25:33 +0900

    Test: P7_AnimateList.py

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -1,9 +1,33 @@
 import sys
 
-sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
-sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
+# sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
+# sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
 
-print(".bash_aliases Test run")
+import webbrowser
+from mal import Anime, AnimeSearch
+
+animeList = list(input("Enter Anime: ").split(","))
+
+for entry in animeList:
+    search = AnimeSearch(entry.lstrip())
+
+    anime = Anime(malID := search.results[0].mal_id)
+    aniEN = anime.title_english
+    aniJA = anime.title_japanese
+    aniTP = anime.type
+    aniUR = anime.url
+
+    print(f'\n{{\n  "mal_id": {malID}')
+    print(f'  "en_title": "{aniEN}"')
+    print(f'  "ja_title": "{aniJA}"')
+    print(f'  "subtitle": ""')
+    print(f'  "type": "{aniTP}"')
+    print(f'  "count": \n}}')
+    print(f"\nCheck: {aniUR}")
+
+    animeURL = input("\nOpen URL in browser? (Y/N): ")
+    if animeURL.lower() in ["y", "yes"]:
+        webbrowser.open_new_tab(aniUR)
