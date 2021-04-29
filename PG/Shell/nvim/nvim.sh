b677f27d6579df9787eddfda99ef6aa32e93058f
s20016 <s20016@std.it-college.ac.jp>
2021-04-29 18:13:25 +0900

    Fix: Typo

--- PG/Shell/nvim/nvim.sh
+++ PG/Shell/nvim/nvim.sh
@@ -20,11 +20,11 @@ fi
 curl -fLo "$_PLUGIN_DIR/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp ./conf/{init.vim,local_bundles.vim,local_init.vim} $_CONFIG_DIR
+cp $HOME/PROJECTS/PG/Shell/nvim/conf/{init.vim,local_bundles.vim,local_init.vim} $_CONFIG_DIR
 
 
 # Python3 Linter
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 pip3 install flake8 autopep8 black isort
 echo -e "${GREEN}Updated: Python3 Settings${RESET}"
 
