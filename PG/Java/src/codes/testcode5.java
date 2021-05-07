c78ebfc7edff3407704821f1047a128ac486ba38
s20016 <s20016@std.it-college.ac.jp>
2021-05-07 12:39:55 +0900

    Lesson: Java 0507

--- PG/Java/src/codes/testcode5.java
+++ PG/Java/src/codes/testcode5.java
@@ -29,3 +29,44 @@ public class testcode5 {
 		}
 	}
 }
+
+// public class testcode5 {
+// 	public static void main(String[] args) {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		String s = null;
+// 		try {
+// 			s = br.readLine();
+// 		} catch (IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 		System.out.println(s);
+// 	}
+// }
+
+// public class testcode5 {
+// 	public static void main(String[] args) {
+// 		Console con = System.console();
+//     	System.out.println("Enter something: ");
+//     	String msg = con.readLine();
+    	
+//     	FileOutputStream fos = null;
+//     	try {
+//     		fos = new FileOutputStream("message.ser");
+//     		@SuppressWarnings("resource")
+// 			ObjectOutputStream out = new ObjectOutputStream(fos);
+//     		out.writeObject(msg);
+//     		out.flush();
+//     	} catch (Exception err) {
+//     		System.out.println(err.getMessage());
+//     	} finally {
+//     		if (fos != null) {
+//     			try {
+//     				fos.close();
+//     			} catch (IOException err) {
+//     				System.out.println(err.getMessage());
+//     			}
+//     		}
+//     	}
+// 	}
+// }
+
