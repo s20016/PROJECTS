a698dce136701bc1e6e9d314c50c0d6fa0c115da
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-05-18 19:59:47 +0900

    Test: img for new init.vim

--- PG/Python/Codes/testcode3.py
+++ PG/Python/Codes/testcode3.py
@@ -6,14 +6,62 @@ sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # TEST CODE 3
 # =========================================================================
 
-x = list(sys.stdin.read().split("\n"))
+from mal import Anime, AnimeSearch, Manga, MangaSearch
+import webbrowser
 
-l = []
+url = []
+animateOption = input(f"Animate list for Anime/Manga? (A/M): ")
 
-for i in x:
-    if "type" in i:
-        i.replace(",", ",\n")
-        print('  "status": "FR",')
-    else: print(i)
+if animateOption.lower() in ["a", "anime"]:
+    animeList = list(input("Enter Anime: ").split(",")); print()
 
-# print("\n".join(l))
+    for entry in animeList:
+        search = AnimeSearch(entry.lstrip())
+
+        anime = Anime(malID := search.results[0].mal_id)
+        aniEN = anime.title_english
+        aniJA = anime.title_japanese
+        aniTP = anime.type
+        url.append(aniUR := anime.url)
+
+        print(f'{{\n  "mal_id": {malID},')
+        print(f'  "en_title": "{aniEN}",')
+        print(f'  "ja_title": "{aniJA}",')
+        print(f'  "subtitle": "",')
+        print(f'  "type": "{aniTP}",')
+        print(f'  "count": 1\n}},')
+    
+    print()
+    url2 = [print(f"{animeList[count].lstrip()}: {url}") for count, url in enumerate(url)]
+    animeURL = input(f"\nOpen {len(animeList)} URL in browser? (Y/N): ")
+    if animeURL.lower() in ["y", "yes"]:
+        for i in url: webbrowser.open_new_tab(i)
+
+elif animateOption.lower() in ["m", "manga"]:
+    mangaList = list(input("Enter Manga: ").split(",")); print()
+
+    for entry in mangaList:
+        search = MangaSearch(entry.lstrip())
+
+        manga = Manga(malID := search.results[0].mal_id)
+        manEN = manga.title_english
+        manJA = manga.title_japanese
+        url.append(manUR := manga.url)
+
+        print(f'{{\n  "mal_id": {malID},')
+        print(f'  "en_title": "{manEN}",')
+        print(f'  "ja_title": "{manJA}",')
+        print(f'  "subtitle": "",')
+        print(f'  "status": "CR",')
+        print(f'  "count": 1\n}},')
+
+    print()
+    url2 = [print(f"{mangaList[count].lstrip()}: {url}") for count, url in enumerate(url)]
+    animeURL = input(f"\nOpen {len(mangaList)} URL in browser? (Y/N): ")
+    if animeURL.lower() in ["y", "yes"]:
+        for i in url: webbrowser.open_new_tab(i)
+else:
+    print("Option Not Available")
+
+# Requirements: 
+# pip install mal-api
