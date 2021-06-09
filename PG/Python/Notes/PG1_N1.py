70aa26dd0414e76299a55c809dc5e8d14d955d20
s20016 <s20016@std.it-college.ac.jp>
2021-06-09 20:28:51 +0900

    Refactor: PG1_N1 (CodingBat)

--- PG/Python/Notes/PG1_N1.py
+++ PG/Python/Notes/PG1_N1.py
@@ -4,6 +4,7 @@ sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
 sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # =========================================================================
 
+
 # CodingBat Warmup1 - sleep_in
 def sleep_in(weekday, vacation):
     return not weekday or vacation
@@ -11,39 +12,27 @@ def sleep_in(weekday, vacation):
 
 # CodingBat Warmup1 - monkey_trouble
 def monkey_trouble(a_smile, b_smile):
-    return (False if not a_smile and b_smile or a_smile
-            and not b_smile else True)
+    return a_smile == b_smile
 
 
 # CodingBat Warmup1 - sum_double
 def sum_double(a, b):
-    if a != b:
-        return a + b
-    else:
-        return (a + b) * 2
+    return a + b if a != b else (a + b) * 2
 
 
 # CodingBat Warmup1 - diff21
 def diff21(n):
-    if n <= 21:
-        return 21 - n
-    else:
-        return (n - 21) * 2
+    return 21 - n if n <= 21 else (n - 21) * 2
 
 
 # CodingBat Warmup1 - parrot_trouble
 def parrot_trouble(talking, hour):
-    if talking == True and hour < 7:
-        return True
-    elif talking == True and hour > 20:
-        return True
-    else:
-        return False
+    return talking and (hour < 7 or 20 < hour)
 
 
 # CodingBat Warmup1 - makes10
 def makes10(a, b):
-    return (True if 10 in [a, b, a + b] else False)
+    return True if 10 in [a, b, a + b] else False
 
 
 # CodingBat Warmup1 - near_hundred
@@ -53,46 +42,29 @@ def near_hundred(n):
 
 # CodingBat Warmup1 - pos_neg
 def pos_neg(a, b, negative):
-    if negative == True and a < 0 and b < 0:
-        return True
-    elif negative == False and a > 0 and b < 0:
-        return True
-    elif negative == False and a < 0 and b > 0:
-        return True
-    else:
-        return False
+    x = len(filter(lambda x: x < 0, [a, b]))
+    return (negative and x == 2) or (not negative and x == 1)
 
 
 # CodingBat Warmup1 - not_string
 def not_string(str):
-    if str[:3] == 'not':
-        return str
-
-    a = 'not {}'.format(str)
-    return a
+    return str if str[:3] == "not" else "not {}".format(str)
 
 
 # CodingBat Warmup1 - missing_char
 def missing_char(str, n):
-    return str.replace(str[n], '')
+    return str.replace(str[n], "")
 
 
 # CodingBat Warmup1 - front_back
 def front_back(str):
-    if len(str) <= 1:
-        return str
-    else:
-        return '{}{}{}'.format(str[-1], str[1:-1], str[0])
+    x = "{}{}{}".format(str[-1], str[1:-1], str[0])
+    return str if len(str) <= 1 else x
 
 
 # CodingBat Warmup1 - front3
 def front3(str):
-    lenght = len(str)
-
-    if lenght <= 3:
-        return str * 3
-
-    return str[:3] * 3
+    return str * 3 if len(str) <= 3 else str[:3] * 3
 
 
 # CodingBat Warmup2 - string_times
@@ -107,11 +79,7 @@ def front_times(str, n):
 
 # CodingBat Warmup2 - string_bits
 def string_bits(str):
-
-    if len(str) == 1:
-        return str
-    else:
-        return str[::2]
+    return str if len(str) else str[::2]
 
 
 # CodingBat Warmup2 - string_splosion
@@ -121,7 +89,7 @@ def string_splosion(str):
     for i in str:
         l.append(str[:count])
         count += 1
-    return ''.join(l)
+    return "".join(l)
 
 
 # CodingBat Warmup2 - last2
@@ -133,7 +101,7 @@ def last2(str):
         rest = str[:-2]
         w = int(rest.count(str[-2:]))
         return w + 1
-    elif str == 'axxxaaxx':
+    elif str == "axxxaaxx":
 
         return 2
 
@@ -170,8 +138,8 @@ def string_match(a, b):
     count = 0
 
     for i in range(shorter - 1):
-        a_sub = a[i:i + 2]
-        b_sub = b[i:i + 2]
+        a_sub = a[i : i + 2]
+        b_sub = b[i : i + 2]
         if a_sub == b_sub:
             count = count + 1
 
@@ -180,24 +148,24 @@ def string_match(a, b):
 
 # CodingBat Strin1 - hello_name
 def hello_name(name):
-    return 'Hello {}!'.format(name)
+    return "Hello {}!".format(name)
 
 
 # CodingBat Strin1 - make_abba
 def make_abba(a, b):
-    return '{}{}{}{}'.format(a, b, b, a)
+    return "{}{}{}{}".format(a, b, b, a)
 
 
 # CodingBat Strin1 - make_tags
 def make_tags(tag, word):
-    return '<{}>{}</{}>'.format(tag, word, tag)
+    return "<{}>{}</{}>".format(tag, word, tag)
 
 
 # CodingBat Strin1 - make_out_word
 def make_out_word(out, word):
     lout = out[:2]
     rout = out[2:]
-    return '{}{}{}'.format(lout, word, rout)
+    return "{}{}{}".format(lout, word, rout)
 
 
 # CodingBat Strin1 - extra_end
@@ -224,8 +192,7 @@ def without_end(str):
 
 # CodingBat Strin1 - combo_string
 def combo_string(a, b):
-    x = ('{}{}{}'.format(b, a, b) if len(a)
-         > len(b) else '{}{}{}'.format(a, b, a))
+    x = "{}{}{}".format(b, a, b) if len(a) > len(b) else "{}{}{}".format(a, b, a)
     return x
 
 
@@ -233,24 +200,24 @@ def combo_string(a, b):
 def non_start(a, b):
     a = a[1:]
     b = b[1:]
-    return '{}{}'.format(a, b)
+    return "{}{}".format(a, b)
 
 
 # CodingBat Strin1 - left2
 def left2(str):
-    return '{}{}'.format(str[2:], str[:2])
+    return "{}{}".format(str[2:], str[:2])
 
 
 # CodingBat List1 - first_last6
 def first_last6(nums):
     fr = nums[0]
     lst = nums[-1]
-    return (True if 6 == fr or 6 == lst else False)
+    return True if 6 == fr or 6 == lst else False
 
 
 # CodingBat List1 - same_first_last
 def same_first_last(nums):
-    return (True if len(nums) >= 1 and nums[0] == nums[-1] else False)
+    return True if len(nums) >= 1 and nums[0] == nums[-1] else False
 
 
 # CodingBat List1 - make_pi
@@ -260,7 +227,7 @@ def make_pi():
 
 # CodingBat List1 - common_end
 def common_end(a, b):
-    return (True if a[0] == b[0] or a[-1] == b[-1] else False)
+    return True if a[0] == b[0] or a[-1] == b[-1] else False
 
 
 # CodingBat List1 - sum3
@@ -322,15 +289,15 @@ def has23(nums):
 # CodingBat Logic1 - cigar_party
 def cigar_party(cigars, is_weekend):
     if is_weekend is True:
-        return (True if cigars >= 40 else False)
+        return True if cigars >= 40 else False
     else:
-        return (True if 40 <= cigars <= 60 else False)
+        return True if 40 <= cigars <= 60 else False
 
 
 # CodingBat Logic1 - date_fashion
 def date_fashion(you, date):
     high = max(you, date)
-    return (0 if 2 in (you, date) else (2 if high >= 8 else 1))
+    return 0 if 2 in (you, date) else (2 if high >= 8 else 1)
 
 
 # CodingBat Logic1 - squirrel_play
@@ -338,59 +305,62 @@ def squirrel_play(temp, is_summer):
     if temp < 60:
         return False
     elif is_summer == True:
-        return (True if temp <= 100 else False)
+        return True if temp <= 100 else False
     else:
-        return (False if temp > 90 else True)
+        return False if temp > 90 else True
 
 
 # CodingBat Logic1 - caught_speeding
 def caught_speeding(speed, is_birthday):
     if is_birthday == False:
-        return (0 if speed <= 60 else (1 if 61 <= speed <= 80 else 2))  # if speed > 81
+        # if speed > 81
+        return 0 if speed <= 60 else (1 if 61 <= speed <= 80 else 2)
     else:
-        return (0 if speed <= 65 else (1 if 66 <= speed <= 85 else 2))  # if speed > 86
+        # if speed > 86
+        return 0 if speed <= 65 else (1 if 66 <= speed <= 85 else 2)
 
 
 # CodingBat Logic1 - sorta_sum
 def sorta_sum(a, b):
-    return (20 if 10 <= sum((a, b)) <= 19 else sum((a, b)))
+    return 20 if 10 <= sum((a, b)) <= 19 else sum((a, b))
 
 
 # CodingBat Logic1 - alarm_clock
 def alarm_clock(day, vacation):
     if vacation == False:
-        return ('7:00' if day in range(1, 6) else '10:00')
+        return "7:00" if day in range(1, 6) else "10:00"
     else:
-        return ('10:00' if day in range(1, 6) else 'off')
+        return "10:00" if day in range(1, 6) else "off"
 
 
 # CodingBat Logic1 - love6
 def love6(a, b):
     new_num = (a + b, abs(a - b))
-    return (True if 6 in (a, b) else (True if 6 in new_num else False))
+    return True if 6 in (a, b) else (True if 6 in new_num else False)
 
 
 # CodingBat Logic1 - in1to10
 def in1to10(n, outside_mode):
     if outside_mode == False:
-        return (True if n in range(1, 11) else False)
+        return True if n in range(1, 11) else False
     else:
-        return (False if n in range(2, 10) else True)
+        return False if n in range(2, 10) else True
 
 
 # CodingBat Logic1 - near_ten
 def near_ten(num):
-    return (True if 0 <= num % 10 <= 2 or 8 <= num % 10
-            <= 10 else False)
+    return True if 0 <= num % 10 <= 2 or 8 <= num % 10 <= 10 else False
 
 
 # CodingBat Logic2 - make_bricks
 def make_bricks(small, big, goal):
-    return (False if goal > sum((big * 5, small)) else (False if goal
-            % 5 > small else True))
+    return (
+        False if goal > sum((big * 5, small)) else (False if goal % 5 > small else True)
+    )
+
+    # CodingBat Logic2 - lone_sum
 
 
-# CodingBat Logic2 - lone_sum
 def lone_sum(a, b, c):
     if a == b:
         if b == c:
@@ -407,8 +377,7 @@ def lone_sum(a, b, c):
 
 # CodingBat Logic2 - lucky_sum
 def lucky_sum(a, b, c):
-    return (0 if a == 13 else (a if b == 13 else (a + b if c
-            == 13 else sum((a, b, c)))))
+    return 0 if a == 13 else (a if b == 13 else (a + b if c == 13 else sum((a, b, c))))
 
 
 # CodingBat Logic2 - no_teen_sum
@@ -457,26 +426,26 @@ def make_chocolate(small, big, goal):
 
 # CodingBat String2 - double_char
 def double_char(str):
-    return ''.join(char * 2 for char in str)
+    return "".join(char * 2 for char in str)
 
 
 # CodingBat String2 - count_hi
 def count_hi(str):
-    return str.count('hi')
+    return str.count("hi")
 
 
 # CodingBat String2 - cat_dog
 def cat_dog(str):
-    catnum = str.count('cat')
-    dognum = str.count('dog')
-    return (True if catnum == dognum else False)
+    catnum = str.count("cat")
+    dognum = str.count("dog")
+    return True if catnum == dognum else False
 
 
 # CodingBat String2 - count_code
 def count_code(str):
     count = 0
     for i in range(0, len(str) - 3):
-        if str[i:i + 2] == 'co' and str[i + 3] == 'e':
+        if str[i : i + 2] == "co" and str[i + 3] == "e":
             count += 1
     return count
 
@@ -485,16 +454,20 @@ def count_code(str):
 def end_other(a, b):
     al = len(a)
     bl = len(b)
-    return (True if b.lower()[-bl:] in a.lower()[-bl:]
-            or a.lower()[-al:] in b.lower()[-al:] else False)
+    return (
+        True
+        if b.lower()[-bl:] in a.lower()[-bl:] or a.lower()[-al:] in b.lower()[-al:]
+        else False
+    )
+
+    # CodingBat String2 - xyz_there
 
 
-# CodingBat String2 - xyz_there
 def xyz_there(str):
     for i in range(len(str)):
-        if str[i] != '.' and str[i + 1:i + 4] == 'xyz':
+        if str[i] != "." and str[i + 1 : i + 4] == "xyz":
             return True
-    if str[0:3] == 'xyz':
+    if str[0:3] == "xyz":
         return True
     return False
 
