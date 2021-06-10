0cfd6e04e2f8d8c6f712c7cb5ec95b57786fcee7
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-06-10 12:52:56 +0900

    Test: CodingBat

--- PG/Python/Codes/testcode5.py
+++ PG/Python/Codes/testcode5.py
@@ -6,4 +6,19 @@ sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # TEST CODE 5
 # =========================================================================
 
-print(input())
+
+def last2(str):
+    # if len(str) == 0:
+    #   return 0
+    # elif len(str) == 4:
+    #   rest = str[:-2]
+    #   w = int(rest.count(str[-2:]))
+    #   return w + 1
+    # elif str == "axxxaaxx":
+    #   return 2
+    # rest = str[:-2]
+    # w = int(rest.count(str[-2:]))
+    return str[:-2].count(str[:2])
+
+
+print(last2("hixxhi"))
