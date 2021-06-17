5f58b9504eca609cd03237cf2ebe9c9b64ea027c
s20016 <s20016@std.it-college.ac.jp>
2021-06-17 22:09:15 +0900

    Shift: 210617

--- PG/JavaScript/Codes/testcode4.js
+++ PG/JavaScript/Codes/testcode4.js
@@ -1,1 +1,11 @@
-		
+(stdin => {
+  // define function
+
+  // declare variables
+  const inputs = stdin.toString().split('\n')
+  const [a, b, c] = inputs[0].split(' ').map(Number) // 数値に変換
+
+  // ここに処理を書く
+  const x = (a < b < c) ? 'Yes' : 'No'
+  console.log(x)
+})(require('fs').readFileSync('/dev/stdin', 'utf8'))
