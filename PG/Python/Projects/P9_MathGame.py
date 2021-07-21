cd206bfa6d48843468e16ee39bd7dda4c72fb146
s20016 <s20016@std.it-college.ac.jp>
2021-07-21 17:00:57 +0900

    Lesson: 210721

--- PG/Python/Projects/P9_MathGame.py
+++ PG/Python/Projects/P9_MathGame.py
@@ -3,8 +3,12 @@ import random, functools
 # Get random number
 def set_numbers():
     max_num = int("1" + (random.choice([1, 2, 3]) * "0")) - 1
-    ran_opt = random.choice(["+", "-", "*", "/"])
+    ran_opt = random.choice(["+", "-", "*", "/", "^"])
     ran_num = random.sample(range(1, max_num), 2)
+    if ran_opt == "/": 
+        ran_num = [random.choice(ran_num), random.choice([3, 4, 5, 10, 15, 20, 25])]
+    if ran_opt == "^": 
+        ran_num = [random.choice(range(1, 151)), 2]
     return ran_num, ran_opt
     
 # Question
@@ -14,12 +18,13 @@ def set_question():
     if opt == "+": ans = functools.reduce(lambda a, b: a + b, nums)
     if opt == "-": ans = functools.reduce(lambda a, b: a - b, nums)
     if opt == "*": ans = functools.reduce(lambda a, b: a * b, nums)
-    if opt == "/":
-        # TODO: Strip trailing zeroes in float type answers
-        dnums = [random.choice(nums), random.choice([3, 4, 5, 10, 15, 20, 25])]
-        question = f"{dnums[0]} {opt} {dnums[1]}"
-        ans = "{:.2f}".format(functools.reduce(lambda a, b: a / b, dnums))
+    if opt == "^": ans = functools.reduce(lambda a, b: a ** b, nums)
+    if opt == "/": ans = "{:.2f}".format(functools.reduce(lambda a, b: a / b, nums))
     return question, str(ans)
+
+# Game Over
+def game_over():
+    print(f"\n  Game Over! Score: {score}\n")
 
 # Main
 question, answer = set_question()
@@ -28,11 +33,14 @@ count, score, spc = 0, 0," "; print()
 
 while (health != 1):
     hbar = " ".join(health)
-    # TODO: Catch error if input != int
     usr_ans = str(input(f"  {hbar}{spc * count}  {question}: "))
     if usr_ans == answer:
         question, answer = set_question(); score += 1
+    elif usr_ans == "pass":
+        if len(health) <= 1: game_over(); break
+        question, answer = set_question()
+        health.pop(); count += 2
     else: 
-        if len(health) <= 1: print(f"\n  Game Over! Score: {score}\n"); break
+        if len(health) <= 1: game_over(); break
         else: health.pop(); count += 2
 
