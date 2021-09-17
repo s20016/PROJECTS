a6c584a955cdc68f05f6285d61d86de9b6e6b658
s20016 <s20016@std.it-college.ac.jp>
2021-09-17 21:55:09 +0900

    Fix: Error message

--- RAM/Random/vs_vimtutor.sh
+++ RAM/Random/vs_vimtutor.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Vertical split vimtutor (JA/EN)
-open_vimtutor() {
+open() {
   ja_tutor=$(locate tutor.ja.utf-8)
   en_tutor="${ja_tutor%/*}/tutor"
   vi -O $ja_tutor $en_tutor +"tabdo windo set nu scb"
 }
 
-if [ ! -x "$(command -v locate)" ]; then echo `locate` ; else open_vimtutor; fi
+# Check 'locate' if installed
+abort() { 
+	echo -e "\nCommand 'locate' not found, but can be installed with:
+\nsudo apt install mlocate\n"
+}
+
+if [ ! -x "$(command -v locate)" ]; then abort; else open; fi
