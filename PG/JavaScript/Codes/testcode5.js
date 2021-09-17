1902bf9bd337fca7bf2e5d7b5f892a3f48437b78
s20016 <s20016@std.it-college.ac.jp>
2021-09-17 19:42:55 +0900

    Test: Prettier

--- PG/JavaScript/Codes/testcode5.js
+++ PG/JavaScript/Codes/testcode5.js
@@ -1,5 +1,4 @@
 const lines = require('fs').readFileSync('/dev/stdin', 'utf8').split('\n')
 const x = lines[0]
 
-for (var i = 0; i < x.length; i++) console.log(lines[0][i])
-
+for (let i = 0; i < x.length; i++) console.log(lines[0][i])
