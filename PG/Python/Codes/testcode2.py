6f107a4ed684921691c626bd87b5349fb34a19d2
Czekras <tinio.johnchristian@gmail.com>
2021-10-08 18:14:56 +0900

    Lesson: 211008

--- PG/Python/Codes/testcode2.py
+++ PG/Python/Codes/testcode2.py
@@ -6,7 +6,28 @@ sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
 # TEST CODE 2
 # =========================================================================
 
-import tmp_animate as animate
+# row = number of records, col = data types of each records
+row, col = 3, ["CHAR", "VARCHAR", "INT", "INT", "INT", "INT", "INT", "CHAR"]
 
-for i in range(len(animate.l4)):
-    print(f'{{\n  "mal_id": {animate.l4[i]},\n  "en_title": "{animate.l3[i]}",\n  "ja_title": "{animate.l5[i]}",\n  "subtitle": "",\n  "type": "{animate.l6[i]}",\n  "count": 1\n}},')
+data_value, database = [], []
+data1 = list(map(lambda x: x.lower(), col))
+data2 = [input().split() for _ in range(row)]
+
+for count in range(row):
+    for num, value in enumerate(data2[count]):
+        if data1[num] in ["char", "varchar", "date"]:
+            if value == "(NULL)":
+                data_value.append("NULL")
+            else:
+                data_value.append(f"'{value}'")
+        if data1[num] in ["int"]:
+            data_value.append(value)
+        if num == len(col) - 1:
+            record = ", ".join([x for x in data_value])
+            if count == row - 1:
+                database.append(f"({record});")
+            else:
+                database.append(f"({record})")
+            data_value.clear()
+
+print(*database, sep=",\n")
