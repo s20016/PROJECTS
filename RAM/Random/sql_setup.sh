3072d9b5183fdb9c94c4bc53a09d93edfe1371cf
Czekras <tinio.johnchristian@gmail.com>
2021-10-15 12:43:38 +0900

    Test: sql_setup.sh

--- RAM/Random/sql_setup.sh
+++ RAM/Random/sql_setup.sh
@@ -1,24 +1,78 @@
 #!/bin/bash
 
-if [! command -v vagrant &> /dev/null]; then
-	echo "Command vagrant is not found. Install Vagrant."; exit
+# Setting prerequisites
+vagrantbox_is_installed=true
+virtualbox_is_installed=true
+oracleSQl_is_downloaded=true
+all_set=true
+
+# Setting variables
+oracleSQL_path=$(find $HOME -type f -name LINUX.X64_193000_db_home.zip | head -1)
+
+# Checking prerequisites
+if [[ ! -f $oracleSQL_path ]]; then
+	oracleSQl_is_downloaded=false; all_set=false
 fi
 
-if [! -f ~/Downloads/LINUX.X64_193000_db_home.zip ]; then
-	echo "Please install 「LINUX.X64_193000_db_home.zip」"; exit
+if ! [ -x "$(command -v vagrant)" ]; then
+	vagrantbox_is_installed=false; all_set=false
 fi
 
-sudo apt update && sudo apt upgrade -y
-sudo apt dist-upgrade 
-sudo apt autoremove
+if ! [ -x "$(command -v VBoxManage)" ]; then
+	virtualbox_is_installed=false; all_set=false
+fi
 
-mkdir -p ~/Boxes
-git clone https://github.com/oracle/vagrant-projects ~/Boxes/.
-mv ~/Downloads/LINUX.X64_193000_db_home.zip ~/Boxes/vagrant-projects/OracleDatabase/19.3.0/.
-unzip ~/Downloads/LINUX.X64_193000_db_home.zip -d ~/Boxes/vagrant-boxes/OracleDatabase/19.3.0/.
-cd ~/Boxes/vagrant-projects/OracleDatabase/19.3.0/; vagrant up
+# Confirming prerequisites
+if [ "$vagrantbox_is_installed" == true ]; then
+	echo " [ OK ]: Vagrant"
+else
+	echo " [ NG ]: Vagrant"
+fi
 
-echo "パスワードをメモ:"
-echo "ORACLE PASSWORD FOR SYS, SYSTEM AND PDBADMIN: パスワードが表示される"
-echo "--- Run 'vagrant ssh' to continue."
+if [ "$virtualbox_is_installed" == true ]; then
+	echo " [ OK ]: VBoxManager"
+else 
+	echo " [ NG ]: VBoxManager"
+fi
+
+if [ "$oracleSQl_is_downloaded" == true ]; then
+	echo -e " [ OK ]: LINUX.X64_193000_db_home.zip\n"
+else 
+	echo -e " [ NG ]: LINUX.X64_193000_db_home.zip\n"
+fi
+
+if [ "$all_set" == true ]; then
+	# echo -e " [ OK ]: Files Complete\n"
+	read -p " You're all set. Continue? [Y/n]: " RES
+else
+	# echo -e " [ NG ]: Files Incomplete\n"
+	echo " Please install/download the files. Aborting..."
+	exit
+fi
+
+# Main Installation
+if [[ ${RES,,} == "y" ]]; then
+	echo " Commencing..."
+
+	sudo apt update
+
+	# Directory for vagrant boxes
+	mkdir -p $HOME/Boxes/
+
+	# Cloning vagrant-projects from Oracle
+	if [ ! -d "$HOME/Boxes/vagrant-projects" ]; then
+		git clone https://github.com/oracle/vagrant-projects.git $HOME/Boxes/.
+	fi
+
+	oracle_vagrant="$HOME/Boxes/vagrant-projects/OracleDatabase/19.3.0/"
+	# vagrant global-status | awk '/oracle-19c-vagrant/ {print $1}'
+
+	cd $oracle_vagrant
+	cp $oracleSQL_path ./.
+	unzip $oracle_vagrant -d ./.
+	vagrant up
+
+else
+	echo " Aborting..."
+fi
 
