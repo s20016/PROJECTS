f6bd28a7185531ff938ed92ce3ebceb2d6af176a
Czekras <tinio.johnchristian@gmail.com>
2021-12-16 20:26:24 +0900

    Test: Animate v2

--- PG/Python/Codes/testcode1.py
+++ PG/Python/Codes/testcode1.py
@@ -1,34 +1,43 @@
 import sys
 import webbrowser
 
-from mal import Anime, AnimeSearch
+import requests as req
+# from mal import Anime, AnimeSearch
 
-# sys.stdin = open("/home/czekras/PROJECTS/PG/Python/Codes/input.txt", "r")
-# sys.stdout = open("/home/czekras/PROJECTS/PG/Python/Codes/output.txt", "w")
+# sys.stdin = open("/home/s20016/PROJECTS/PG/Python/Codes/input.txt", "r")
+# sys.stdout = open("/home/s20016/PROJECTS/PG/Python/Codes/output.txt", "w")
 
 # TEST CODE 1
 # =========================================================================
 
+x = []
+url = "https://raw.githubusercontent.com/s20016/Animate/master/public/data/anime_list.json"
 
-animeList = list(input("Enter Anime: ").split(","))
+data = req.get(url).json()['anime']
+for i in data:
+    x.append(i['mal_id'])
 
-for entry in animeList:
-    search = AnimeSearch(entry.lstrip())
+print(x)
 
-    anime = Anime(malID := search.results[0].mal_id)
-    aniEN = anime.title_english
-    aniJA = anime.title_japanese
-    aniTP = anime.type
-    aniUR = anime.url
+# animeList = list(input("Enter Anime: ").split(","))
 
-    print(f'\n{{\n  "mal_id": {malID}')
-    print(f'  "en_title": "{aniEN}"')
-    print(f'  "ja_title": "{aniJA}"')
-    print(f'  "subtitle": ""')
-    print(f'  "type": "{aniTP}"')
-    print(f'  "count": \n}}')
-    print(f"\nCheck: {aniUR}")
+# for entry in animeList:
+#     search = AnimeSearch(entry.lstrip())
 
-    animeURL = input("\nOpen URL in browser? (Y/N): ")
-    if animeURL.lower() in ["y", "yes"]:
-        webbrowser.open_new_tab(aniUR)
+#     anime = Anime(malID := search.results[0].mal_id)
+#     aniEN = anime.title_english
+#     aniJA = anime.title_japanese
+#     aniTP = anime.type
+#     aniUR = anime.url
+
+#     print(f'\n{{\n  "mal_id": {malID}')
+#     print(f'  "en_title": "{aniEN}"')
+#     print(f'  "ja_title": "{aniJA}"')
+#     print(f'  "subtitle": ""')
+#     print(f'  "type": "{aniTP}"')
+#     print(f'  "count": \n}}')
+#     print(f"\nCheck: {aniUR}")
+
+#     animeURL = input("\nOpen URL in browser? (Y/N): ")
+#     if animeURL.lower() in ["y", "yes"]:
+#         webbrowser.open_new_tab(aniUR)
