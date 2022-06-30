493436a0cb31d4d8e02b18aed0ff316c4fa6e7b9
JC TINIO <s20016@std.it-college.ac.jp>
2022-06-30 11:34:30 +0900

    Test: Python code

--- PG/JavaScript/Codes/testcode2.js
+++ PG/JavaScript/Codes/testcode2.js
@@ -1,10 +1,6 @@
 // Read Input
 const lines = require('fs').readFileSync('/dev/stdin', 'utf8').split('\n')
-const [d, s] = lines[0].split(' ').map(Number)
+const [A, B] = lines[0].split(' ').map(Number)
 
 // Code Here
-const x = (d * 1000 * 100) / s >= 10000 ? 'Yes' : 'No'
-
-console.log(x)
-
-
+console.log(A && B)
--- PG/JavaScript/Codes/testcode5.js
+++ PG/JavaScript/Codes/testcode5.js
@@ -1,11 +1,7 @@
 const stream = require('fs').readFileSync('/dev/stdin', 'utf8').trim()
 const lines = stream.split('\n')
 
-// console.log(lines)
+const [A, B, C, D] = lines[0].split(' ').map(Number)
+const X = !((!A && !B) || !C) ^ D
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i)
-// }
-
-const x = [...Array(5).map((v, i) => v + 1)]
-console.log(x)
+console.log(Number(X))
