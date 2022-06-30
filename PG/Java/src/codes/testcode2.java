87bdb101b63d9304f2fd14f9717ba0ccd306b40d
JC TINIO <s20016@std.it-college.ac.jp>
2022-06-30 11:33:52 +0900

    Test: Java code

--- PG/Java/src/codes/testcode2.java
+++ PG/Java/src/codes/testcode2.java
@@ -1,9 +1,225 @@
 package codes;
 
-import java.io.Console;
+import java.util.Scanner;
 
-public class testcode2 {
-	public static void main(String[] args) {
-        System.out.println("TEST 2");
-	}
+public class KnapsackGA {
+  public static final int KNAP_MAX = 6;		// ナップサックの耐重量
+  public static final int ITEM_NUM = 5;		// 品物の種類
+
+  public static final int IND_NUM = 8;		// 個体の数
+  public static final double MUTATE_RATE = 0.1;	// 突然変異する確率（10％）
+
+  public static char[] itemName = { 'A', 'B', 'C', 'D', 'E' };	// 品物の名称
+  public static int[] itemWeight = { 1, 2, 3, 4, 5 };		// 品物の重量
+  public static int[] itemValue = { 100, 300, 350, 500, 650 };	// 品物の価値
+
+  public static int indGeneration;				// 個体の世代
+  public static int[][] indGene = new int[IND_NUM][ITEM_NUM];	// 個体の遺伝子
+  public static int[] indWeight = new int[IND_NUM];		// 個体の重量
+  public static int[] indValue = new int[IND_NUM];		// 個体の価値
+  public static int[] indFitness = new int[IND_NUM];		// 個体の適応度
+
+  // 個体をランダムに生成するメソッド
+  public static void createIndividual() {
+    int ind, item;	// ループカウンタ
+
+    // 0か1をランダムに格納する
+    for (ind = 0; ind < IND_NUM; ind++) {
+      for (item = 0; item < ITEM_NUM; item++) {
+        indGene[ind][item] = Math.random() > 0.5 ? 0 : 1;
+      }
+    }
+  }
+
+  // 個体の重量、価値、適応度を計算するメソッド
+  public static void calcIndividual() {
+    int ind, item;	// ループカウンタ
+
+    for (ind = 0; ind < IND_NUM; ind++) {
+      // 重量と価値を計算する
+      indWeight[ind] = 0;
+      indValue[ind] = 0;
+      for (item = 0; item < ITEM_NUM; item++) {
+        if (indGene[ind][item] == 1) {
+          indWeight[ind] += itemWeight[item];
+          indValue[ind] += itemValue[item];
+        }
+      }
+
+      // 適応度を計算する
+      if (indWeight[ind] <= KNAP_MAX) {
+        // 耐重量以下なら、価値をそのまま適応度とする
+        indFitness[ind] = indValue[ind];
+      }
+      else {
+        // 耐重量を超えているなら、適応度を0とする
+        indFitness[ind] = 0;
+      }
+    }
+  }
+
+  // 個体の情報を表示するメソッド
+  public static void showIndividual() {
+    int ind, item;	// ループカウンタ
+
+    // 世代を表示する
+    System.out.printf("\n＜第%d世代＞\n", indGeneration);
+
+    // 遺伝子、重量、価値、適応度を表示する
+    System.out.printf("遺伝子\t\t重量\t価値\t適応度\n");
+    for (ind = 0; ind < IND_NUM; ind++) {
+      for (item = 0; item < ITEM_NUM; item++) {
+        System.out.printf("[%d]", indGene[ind][item]);
+      }
+      System.out.printf("\t%2dkg\t%4d円\t%4d\n", indWeight[ind], indValue[ind], indFitness[ind]);
+    }
+    System.out.printf("\n");
+  }
+
+  // 個体を適応度の大きい順にソートするメソッド
+  public static void sortIndividual() {
+    int pos;	// 挿入する要素
+    int ins;	// 挿入する位置
+    int item;	// ループカウンタ
+    int tmp;	// 一時的に値を逃がす変数
+
+    // 挿入法でソートする
+    for (pos = 1; pos < IND_NUM; pos++) {
+      ins = pos;
+      while (ins >= 1 && indFitness[ins - 1] < indFitness[ins]) {
+        for (item = 0; item < ITEM_NUM; item++) {
+          tmp = indGene[ins - 1][item];
+          indGene[ins - 1][item] = indGene[ins][item];
+          indGene[ins][item] = tmp;
+        }
+
+        tmp = indWeight[ins - 1];
+        indWeight[ins - 1] = indWeight[ins];
+        indWeight[ins] = tmp;
+
+        tmp =  indValue[ins - 1];
+        indValue[ins - 1] = indValue[ins];
+        indValue[ins] = tmp;
+
+        tmp = indFitness[ins - 1];
+        indFitness[ins - 1] = indFitness[ins];
+        indFitness[ins] = tmp;
+
+        ins--;
+      }
+    }
+  }
+
+  // 淘汰するメソッド
+  public static void selectIndividual() {
+    int ind, item;	// ループカウンタ
+
+    // 適応度の上位50％を下位50％にコピーする（下位50％を淘汰する）
+    for (ind = 0; ind < IND_NUM / 2; ind++) {
+      for (item = 0; item < ITEM_NUM; item++) {
+        indGene[ind + IND_NUM / 2][item] = indGene[ind][item];
+      }
+    }
+    System.out.printf("下位50％を淘汰しました。\n");
+  }
+
+  // 交叉するメソッド
+  public static void crossoverIndividual() {
+    int ind, item;	// ループカウンタ
+    int crossoverPoint;	// 交叉する位置
+    int tmp;		// 一時的に値を逃がす変数
+
+    // 下位50％にコピーした個体を対象とする
+    for (ind = IND_NUM / 2; ind < (IND_NUM - 1); ind += 2) {
+      // 交叉する位置をランダムに決める
+      crossoverPoint = (int)(Math.random() * 10000) % (ITEM_NUM - 1) + 1;
+      for (item = crossoverPoint; item < ITEM_NUM; item++) {
+        // 隣の個体と交差する
+        tmp = indGene[ind][item];
+        indGene[ind][item] = indGene[ind + 1][item];
+        indGene[ind + 1][item] = tmp;
+      }
+      System.out.printf("個体%dと個体%dを%dの位置で交叉しました。\n", ind, ind + 1, crossoverPoint);
+    }
+  }
+
+  // 突然変異するメソッド
+  public static void mutateIndividual() {
+    int ind, item;	// ループカウンタ
+
+    // 下位50％にコピーした個体を対象とする
+    for (ind = IND_NUM / 2; ind < IND_NUM; ind++) {
+      for (item = 0; item < ITEM_NUM; item++) {
+        // あらかじめ決められた確率で突然変異する
+        if (Math.random() <= MUTATE_RATE) {
+          // 反転する
+          indGene[ind][item] ^= 1;
+          System.out.printf("個体%dの%dの位置で突然変異しました。\n", ind, item);
+        }
+      }
+    }
+  }
+
+  // プログラムの実行開始位置となるmainメソッド
+  public static void main(String[] args) {
+    int genMax;	// 最大の世代
+    int item;	// ループカウンタ
+
+    // 最大の世代をキー入力する
+    Scanner scn = new Scanner(System.in);
+    System.out.printf("最大の世代 = ");
+    genMax = scn.nextInt();
+
+    // 第1世代の個体を生成する
+    indGeneration = 1;
+    createIndividual();
+
+    // 適応度を計算する
+    calcIndividual();
+
+    // 適応度が大きい順にソートする
+    sortIndividual();
+
+    // 個体を表示する
+    showIndividual();
+
+    // 1世代ずつ進化させる
+    indGeneration++;
+    while (indGeneration <= genMax) {
+      // 適応度が大きい順にソートする
+      sortIndividual();
+
+      // 淘汰する
+      selectIndividual();
+
+      // 交叉する
+      crossoverIndividual();
+
+      // 突然変異する
+      mutateIndividual();
+
+      // 適応度を計算する
+      calcIndividual();
+
+      // 適応度が大きい順にソートする
+      sortIndividual();
+
+      // 個体を表示する
+      showIndividual();
+
+      // 世代を進める
+      indGeneration++;
+    }
+
+    // 最も適応度の高い個体を解として表示する
+    System.out.printf("＜ナップサックに入っている品物を表示する＞\n");
+    for (item = 0; item < ITEM_NUM; item++) {
+      if (indGene[0][item] == 1) {
+        System.out.printf("%c, %dkg, %d円\n", itemName[item], itemWeight[item], itemValue[item]);
+      }
+    }
+    System.out.printf("\n＜解を表示する＞\n");
+    System.out.printf("重量の合計値 = %dkg\n", indWeight[0]);
+    System.out.printf("価値の最大値 = %d円\n", indValue[0]);
+  }
 }
--- PG/Java/src/codes/testcode4.java
+++ PG/Java/src/codes/testcode4.java
@@ -1,7 +1,150 @@
 package codes;
 
-public class testcode4 {
-	public static void main(String[] args) {
-        System.out.println("TEST 4");
-	}
+public class KnapsackDP {
+  // ナップサックの耐重量
+  public static final int KNAP_MAX = 6;
+
+  // 品物の種類
+  public static final int ITEM_NUM = 5;
+
+  // 品物の名称
+  public static char[] name = { 'A', 'B', 'C', 'D', 'E' };
+
+  // 品物の重量
+  public static int[] weight = { 1, 2, 3, 4, 5 };
+
+  // 品物の価値
+  public static int[] value = { 100, 300, 350, 500, 650 };
+
+  // 品物を吟味した直後の価値
+  public static int[][] maxValue = new int[ITEM_NUM][KNAP_MAX + 1];
+
+  // 最後に入れた品物
+  public static int[] lastItem = new int[KNAP_MAX + 1];
+
+  // item番目の品物を吟味した直後のナップザックの内容を表示するメソッド
+  public static void showKnap(int item) {
+    int knap;	// 0kg～6kgのナップサックを指す
+
+    // 吟味した品物の情報を表示する
+    System.out.printf("＜%c, %dkg, %d円を吟味した結果＞\n", name[item], weight[item], value[item]);
+
+    // ナップサックの耐重量を表示する
+    for (knap = 0; knap <= KNAP_MAX; knap++) {
+      System.out.printf("%dkg\t", knap);
+    }
+    System.out.printf("\n");
+
+    // ナップサックに詰められた品物の価値の合計を表示する
+    for (knap = 0; knap <= KNAP_MAX; knap++) {
+      System.out.printf("%d円\t", maxValue[item][knap]);
+    }
+    System.out.printf("\n");
+
+    // ナップサックに最後に入れた品物を表示する
+    for (knap = 0; knap <= KNAP_MAX; knap++) {
+      if (lastItem[knap] != -1) {
+        System.out.printf("%c\t", name[lastItem[knap]]);
+      }
+      else {
+        System.out.printf("なし\t");
+      }
+    }
+    System.out.printf("\n\n");
+  }
+
+  // プログラムの実行開始位置となるmainメソッド
+  public static void main(String[] args) {
+    int item;		// 品物の番号
+    int knap;		// 0kg～6kgのナップサックを指す
+    int selVal;		// 仮に品物を選んだ場合の価値の合計値
+    int totalWeight;	// 重量の合計値
+
+    // 0番目の品物を吟味する
+    item = 0;
+    // 0kg～KNAP_MAXkgのナップサックで吟味する
+    for (knap = 0; knap <= KNAP_MAX; knap++) {
+      // 耐重量以下なら選ぶ
+      if (weight[item] <= knap) {
+        maxValue[item][knap] = value[item];
+        lastItem[knap] = item;
+      }
+      // 耐重量以下でないなら選ばない
+      else {
+        maxValue[0][knap] = 0;
+        lastItem[knap] = -1;
+      }
+    }
+    showKnap(item);
+
+    // 1番目～ITEM_NUM-1番目の品物を吟味する
+    for (item = 1; item < ITEM_NUM; item++) {
+      // 0kg～KNAP_MAXkgのナップサックで吟味する
+      for (knap = 0; knap <= KNAP_MAX; knap++) {
+        // 耐重量以下の場合
+        if (weight[item] <= knap) {
+          // 選んだ場合の価値を求めてみる
+          selVal = maxValue[item - 1][knap - weight[item]] + value[item];
+          // 価値が大きくなるなら選ぶ
+          if (selVal > maxValue[item - 1][knap]) {
+            maxValue[item][knap] = selVal;
+            lastItem[knap] = item;
+          }
+          // 価値が大きくならないなら選ばない
+          else {
+            maxValue[item][knap] = maxValue[item - 1][knap];
+          }
+        }
+        // 耐重量以下でないなら選ばない
+        else {
+          maxValue[item][knap] = maxValue[item - 1][knap];
+        }
+      }
+      showKnap(item);
+    }
+
+    // ナップサックに入っている品物を調べて、解を表示する
+    System.out.printf("＜ナップサックに入っている品物を調べる＞\n");
+    totalWeight = 0;
+    for (knap = KNAP_MAX; knap > 0; knap -= weight[item]) {
+      item = lastItem[knap];
+      System.out.printf("%dkgのナップサックに最後に入れた品物は%cです。\n", knap, name[item]);
+      totalWeight += weight[item];
+      System.out.printf("　%c, %dkg, %d円\n", name[item], weight[item], value[item]);
+      System.out.printf("　%dkg - %dkg = %dkgです。\n", knap, weight[item], knap - weight[item]);
+    }
+    System.out.printf("\n＜解を表示する＞\n");
+    System.out.printf("重量の合計値 = %dkg\n", totalWeight);
+    System.out.printf("価値の最大値 = %d円\n", maxValue[ITEM_NUM - 1][KNAP_MAX]);
+  }
+}
+
+public class KnapsackGreedy {
+  public static void main(String[] args) {
+    final int KNAP_MAX = 6;	// ナップサックの耐重量
+    final int ITEM_NUM = 5;	// 品物の種類
+    int totalWeight = 0;	// 重量の合計
+    int totalValue = 0;
+
+    // 品物の情報（価値の大きい順にソート済み）
+    char[] name = { 'E', 'D', 'C', 'B', 'A' };
+    int[] weight = { 5, 4, 3, 2, 1 };
+    int[] value = { 650, 500, 350, 300, 100};
+
+    // 価値の大きい順に選ぶ
+    for (int i = 0; i < ITEM_NUM; i++) {
+      if (totalWeight + weight[i] <= KNAP_MAX) {
+        System.out.printf("品物%cを選ぶ。\n", name[i]);
+        totalWeight += weight[i];
+        totalValue += value[i];
+      }
+      else {
+        break;
+      }
+    }
+
+    // 結果を表示する
+    System.out.printf("重量の合計値 = %dkg\n", totalWeight);
+    System.out.printf("価値の合計値 = %d円\n", totalValue);
+  }
 }
